fd93f9779bb659853c5eed99a6137020081c476c
Thomas van Tilburg <0929444@hr.nl>
2017-06-08 19:30:06 +0200

    Update URL

--- RESTAPI/src/main/java/me/thomasvt/bankingserver/Bank.java
+++ RESTAPI/src/main/java/me/thomasvt/bankingserver/Bank.java
@@ -2,7 +2,7 @@ package me.thomasvt.bankingserver;
 
 public class Bank {
 
-	BankObject SOFA = new BankObject("https://api.bank.thomasvt.xyz", "aae45b32-4a99-11e7-9f16-de2b444c2004",
+	BankObject SOFA = new BankObject("https://api.sofabank.ml", "aae45b32-4a99-11e7-9f16-de2b444c2004",
 			"72fb3847-f197-4f6e-a471-78b2fc5b7e1a");
 
 	public class BankObject {
